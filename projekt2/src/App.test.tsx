d7fb7e5483aaf918b4b6c4a47dff11b0d1d214a5
codeninjah <codeninjahblog@gmail.com>
2021-12-03 15:48:05 +0100

    First tests

--- projekt2/src/App.test.tsx
+++ projekt2/src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow, mount } from 'enzyme';
+import { ProductItem, ProductList } from './App';
+import { Products } from "./database.json"
+//import { renderIntoDocument } from 'react-dom/test-utils';
 
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
+const product = Products[0]
+describe("Finns grid av produkter", () => {
+  it("renders ProductItem without errors (smoke test)", () => {
+    render(<ProductItem name={product.name} id={product.id} price={product.price} />)
+    const nameElement = screen.getByText(/Apple/i)
+    expect(nameElement).toBeInTheDocument()
+  })
+  it("renders ProductList without errors (smoke test)", () => {
+    render(<ProductList/>)
+    const nameElement = screen.getByText(/Kiwi/i)
+    expect(nameElement).toBeInTheDocument()
+  })
+});
+
