7eda19395161ae1843007df2745c81d19228d5b3
John Doe <soold.simon@gmail.com>
2021-12-03 14:42:46 +0100

    products

--- projekt2/src/App.tsx
+++ projekt2/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Products } from "./database.json"
+
+interface Product {
+  id: string,
+  name: string,
+  price: number
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductList />
     </div>
   );
+}
+
+const ProductItem = ({id, name, price}:Product) => {
+  const BuyItem = () => {
+    console.log("buy this item")
+  }
+  return (
+    <div key={id}>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <button onClick={BuyItem}>Buy</button>
+    </div>
+  ) 
+}
+
+const ProductList = () => {
+  const itemList = Products
+  const [inputValue, setInputValue] = useState<string>("")
+  return (
+    <>
+    <input 
+    type="text"
+    value={inputValue}
+    onChange={(
+      ev: React.ChangeEvent<HTMLInputElement>,
+    ): void => setInputValue(ev.target.value)}
+    />
+    <p>{inputValue}</p>
+    {
+      itemList.filter(item => item.name.toLowerCase().includes(inputValue.toLowerCase())).map(item => (
+        <ProductItem id={item.id} name={item.name} price={item.price}/>
+      ))
+    }
+    </>
+  )
 }
 
 export default App;
